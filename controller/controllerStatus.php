bd0d8fc374c513629b6a70ed3e1cd3cf1d5937b5
daniel_santana_86 <daniel_santana_87@hotmail.com>
2024-09-10 20:12:56 -0300

    cirando controllerStatus

---
+++ controller/controllerStatus.php
@@ -0,0 +1,64 @@
+<?php 
+class controllerStatus{
+    
+    //Listar todos os Status
+    public function getAll(){
+        try {
+            $modelStatus = new modelStatus();
+            return $modelStatus->getAll();
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    //Listar status por ID
+    public function getById($idStatus){
+        try {
+            $modelStatus = new modelStatus();
+            return $modelStatus->getById($idStatus);
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    //Inserir um novo Status
+    public function save($data){
+        try {
+            
+            $modelStatus = new modelStatus();
+            return $modelStatus->save($data);
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    //Atualizar um status existente
+    public function update($idStatus, $data){
+        try {
+
+            $modelStatus = new modelStatus();
+            return $modelStatus->update($idStatus, $data);
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    //Deletar um status existente
+    public function dele($idStatus){
+        try {
+            $modelStatus = new modelStatus();
+            return $modelStatus->delete($idStatus);
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+
+
+
+}
+
+?>
