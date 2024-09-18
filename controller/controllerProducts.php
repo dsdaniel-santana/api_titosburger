69e8c3067ac9017ef0a93d67a6df53f31870b736
daniel_santana_86 <daniel_santana_87@hotmail.com>
2024-09-17 21:57:11 -0300

    finished categories

---
+++ controller/controllerProducts.php
@@ -0,0 +1,69 @@
+<?php
+class controllerProducts {
+    
+    //Criar novos produtos
+    public function save($data) {
+        try {
+            
+            $modelProducts = new modelProducts();
+            return $modelProducts->save($data);
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    //Listar todos os produtos
+    public function listAll(){
+        try {
+            $modelProducts = new modelProducts();
+            return $modelProducts->listAll(); 
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    //Listar um produto por ID
+    public function searchById($id){
+        try {
+            $modelProducts = new modelProducts();
+            return $modelProducts->searchById($id); 
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    //Listar os produtos de uma categoria
+    public function listByCategory($id){
+        try {
+            $modelProducts = new modelProducts();
+            return $modelProducts->listByCategory($id); 
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    //Atualizar um produto por ID
+    public function update($id, $data){
+        try {
+            $modelProducts = new modelProducts();
+            return $modelProducts->update($id, $data); 
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+
+    //Deletar um produo por ID
+    public function delete($id){
+        try {
+            $modelProducts = new modelProducts();
+            return $modelProducts->delete($id); 
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+    
+}
+
+?>
--- db/tblProdutcts.sql
+++ db/tblProdutcts.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS tblProducts(
     id_product INT PRIMARY KEY AUTO_INCREMENT,
     product_name VARCHAR(10) NOT NULL,
+    image VARCHAR(50)NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     description VARCHAR(50) NOT NULL,
     id_category INT NOT NULL,
